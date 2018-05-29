1b2133835af9de12cc345e565e8788159811a6e3
Daniel Souza Affonso Ferreira <daniel.souza.af@gmail.com>
2018-05-29 10:19:08 -0300

    ok

---
+++ jantar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <semaphore.h>
+
+#define NUM 5
+
+#define DIREITA(id+1)%NUM
+
+#define PENSANDO 0
+#define FAMINTO 1
+#define COMENDO 2
+
+sem_t mutex = 1;
+
+void filosofo(int id) {
+	while (1 == 1) {
+		pensar();
+		down(&mutex);
+		pegar_garfo(id);
+		pegar_garfo(direita);
+		comer();
+		largar_garfo(id);
+		largar_garfo(direita);
+		up(&mutex);
+	}
+	}
